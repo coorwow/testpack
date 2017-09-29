672bb69138e72ab7d20c33f374a2fe2c588b8761
guoxiaofei <gxfstream@gmail.com>
2017-09-29 14:36:14 +0800

    add test2

--- src/testClass.php
+++ src/testClass.php
@@ -3,8 +3,13 @@ namespace test;
 class testClass{
 
     public function testClass(){
-        echo "this is testClass";
+        echo "this is testClassv2.0";
     }
+
+    public function test2(){
+        echo "this is test2";
+    }
+
 
 }
 ?>
